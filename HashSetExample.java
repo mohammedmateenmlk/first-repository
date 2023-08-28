8ebab189f211d125ac7ad26e4c542121137510b4
Mohammed Mateen <122994530+mohammedmateenmlk@users.noreply.github.com>
2023-08-28 18:53:03 +0530

    first commit

---
+++ HashSetExample.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+public static void main(String[] args) {
+	HashSet<Integer> hs=new HashSet<Integer>();
+	hs.add(123);
+	hs.add(321);
+	hs.add(147);
+	hs.add(258);
+	
+	System.out.println(hs);
+}
+}
